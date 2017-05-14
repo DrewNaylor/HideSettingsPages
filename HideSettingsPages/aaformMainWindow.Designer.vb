8848520dd5ac8ae060169aa543413fb26e92aa58
Drew Naylor <DrewNaylor@users.noreply.github.com>
2017-05-14 19:02:10 -0400

    Add radio buttons

--- HideSettingsPages/aaformMainWindow.Designer.vb
+++ HideSettingsPages/aaformMainWindow.Designer.vb
@@ -29,7 +29,11 @@ Partial Class aaformMainWindow
         Me.menubarExitButton = New System.Windows.Forms.ToolStripMenuItem()
         Me.menubarHelpMenu = New System.Windows.Forms.ToolStripMenuItem()
         Me.menubarAboutButton = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
         Me.flowlayoutpanelMainWindow.SuspendLayout()
+        Me.groupboxSettingsPageVisibility.SuspendLayout()
         Me.menubarMainWindow.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -44,6 +48,9 @@ Partial Class aaformMainWindow
         '
         'groupboxSettingsPageVisibility
         '
+        Me.groupboxSettingsPageVisibility.Controls.Add(Me.RadioButton2)
+        Me.groupboxSettingsPageVisibility.Controls.Add(Me.RadioButton1)
+        Me.groupboxSettingsPageVisibility.Controls.Add(Me.Label1)
         Me.groupboxSettingsPageVisibility.Location = New System.Drawing.Point(3, 3)
         Me.groupboxSettingsPageVisibility.Name = "groupboxSettingsPageVisibility"
         Me.groupboxSettingsPageVisibility.Size = New System.Drawing.Size(323, 104)
@@ -86,6 +93,37 @@ Partial Class aaformMainWindow
         Me.menubarAboutButton.Size = New System.Drawing.Size(152, 22)
         Me.menubarAboutButton.Text = "About"
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(10, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(283, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Choose whether to hide pages or show only certain pages."
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(13, 47)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(47, 17)
+        Me.RadioButton1.TabIndex = 1
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Hide"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(13, 70)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(76, 17)
+        Me.RadioButton2.TabIndex = 2
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Show Only"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
         'aaformMainWindow
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
@@ -98,6 +136,8 @@ Partial Class aaformMainWindow
         Me.Name = "aaformMainWindow"
         Me.Text = "HideSettingsPages"
         Me.flowlayoutpanelMainWindow.ResumeLayout(False)
+        Me.groupboxSettingsPageVisibility.ResumeLayout(False)
+        Me.groupboxSettingsPageVisibility.PerformLayout()
         Me.menubarMainWindow.ResumeLayout(False)
         Me.menubarMainWindow.PerformLayout()
         Me.ResumeLayout(False)
@@ -112,4 +152,7 @@ Partial Class aaformMainWindow
     Friend WithEvents menubarHelpMenu As ToolStripMenuItem
     Friend WithEvents menubarAboutButton As ToolStripMenuItem
     Friend WithEvents groupboxSettingsPageVisibility As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
 End Class
