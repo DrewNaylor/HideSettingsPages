d145f2d04eff00be5156c7960eab5426e803108d
Drew Naylor <DrewNaylor@users.noreply.github.com>
2017-05-15 22:41:39 -0400

    Made more strings to store the Registry...
    key.

--- HideSettingsPages/VB Code-behind/registryKeyBuilder.vb
+++ HideSettingsPages/VB Code-behind/registryKeyBuilder.vb
@@ -8,7 +8,13 @@
     ' that updates based on which of the radio buttons at the 
     ' top the user clicks.
     Public Shared stringHideOrShowOnly As String
-
+    ' Create a public, shared string called stringPageList
+    ' to store the list of pages the user wants to hide or shor only
+    ' in the Settings app.
+    Public Shared stringPageList As String
+    ' Create a public, shared string called stringFullRegistryKey
+    ' to store the completed Registry key.
+    Public Shared stringFullRegistryKey As String
 
 
 #End Region
