de0ca331840069da6f476d62b1000b6d57ab9746
Drew Naylor <DrewNaylor@users.noreply.github.com>
2017-05-15 22:36:26 -0400

    There's now a string to store the radio button...
    choice that the user clicks.

--- HideSettingsPages/VB Code-behind/registryKeyBuilder.vb
+++ HideSettingsPages/VB Code-behind/registryKeyBuilder.vb
@@ -1,7 +1,14 @@
 ﻿Public Class registryKeyBuilder
-
+#Region "Figure out the Registry key to set to hide pages."
     ' This class has subs that figure out what the Registry key data will be
     ' and it gets updated when the user checks checkboxes or changes the radio
     ' buttons at the top.
 
+    ' Create a public, shared string that updates based on which of
+    ' the radio buttons at the top the user clicks.
+    Public Shared stringHideOrShowOnly As String
+
+
+
+#End Region
 End Class
