cb3f76e8f73404807cf1eda27476b0a13054b347
Drew Naylor <DrewNaylor@users.noreply.github.com>
2017-05-15 22:31:05 -0400

    Add registry key builder code file.

---
+++ HideSettingsPages/VB Code-behind/registryKeyBuilder.vb
@@ -0,0 +1,7 @@
+﻿Public Class registryKeyBuilder
+
+    ' This class has subs that figure out what the Registry key data will be
+    ' and it gets updated when the user checks checkboxes or changes the radio
+    ' buttons at the top.
+
+End Class
