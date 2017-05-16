b15d982a2af3da378014ce566ba090f4dd6f63f6
Drew Naylor <DrewNaylor@users.noreply.github.com>
2017-05-15 22:38:28 -0400

    Update some comment text.

--- HideSettingsPages/VB Code-behind/registryKeyBuilder.vb
+++ HideSettingsPages/VB Code-behind/registryKeyBuilder.vb
@@ -4,8 +4,9 @@
     ' and it gets updated when the user checks checkboxes or changes the radio
     ' buttons at the top.
 
-    ' Create a public, shared string that updates based on which of
-    ' the radio buttons at the top the user clicks.
+    ' Create a public, shared string called stringHideOrShowOnly
+    ' that updates based on which of the radio buttons at the 
+    ' top the user clicks.
     Public Shared stringHideOrShowOnly As String
 
 
