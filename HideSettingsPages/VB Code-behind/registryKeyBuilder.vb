4d367790abca9be57e5bddc95019f02183ce9500
Drew Naylor <DrewNaylor@users.noreply.github.com>
2017-05-19 00:06:40 -0400

    Add boilerplate text to registryKeyBuilder.vb...
    I forgot to do this until just now. Also, I've removed the spaces that
    go in front of the text in the boilerplate text area because it doesn't
    look very good.

--- HideSettingsPages/VB Code-behind/registryKeyBuilder.vb
+++ HideSettingsPages/VB Code-behind/registryKeyBuilder.vb
@@ -1,4 +1,29 @@
-﻿Public Class registryKeyBuilder
+﻿'HideSettingsPages - GUI for setting a Registry key to hide individual pages
+'in the Windows 10 Settings app on the Creators Update and newer.
+'Copyright (C) 2017  Drew Naylor
+'Microsoft Windows and all related words are copyright
+'and trademark Microsoft Corporation.
+'(Note that the copyright years include the years left out by the hyphen.)
+'
+'This file is part of HideSettingsPages
+'(Program is also known as "Hide Windows 10 Settings App Pages.")
+'
+'HideSetingsPages is free software: you can redistribute it and/or modify
+'it under the terms of the GNU General Public License as published by
+'the Free Software Foundation, either version 3 of the License, or
+'(at your option) any later version.
+'
+'HideSettingsPages is distributed in the hope that it will be useful,
+'but WITHOUT ANY WARRANTY; without even the implied warranty of
+'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'GNU General Public License for more details.
+'
+'You should have received a copy of the GNU General Public License
+'along with HideSettingsPages.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+Public Class registryKeyBuilder
 #Region "Figure out the Registry key to set to hide pages."
     ' This class has subs that figure out what the Registry key data will be
     ' and it gets updated when the user checks checkboxes or changes the radio
--- HideSettingsPages/aaformMainWindow.vb
+++ HideSettingsPages/aaformMainWindow.vb
@@ -1,25 +1,25 @@
-﻿' HideSettingsPages - GUI for setting a Registry key to hide individual pages
-'   in the Windows 10 Settings app on the Creators Update and newer.
-' Copyright (C) 2017  Drew Naylor
-' Microsoft Windows and all related words are copyright
-'   and trademark Microsoft Corporation.
-' (Note that the copyright years include the years left out by the hyphen.)
+﻿'HideSettingsPages - GUI for setting a Registry key to hide individual pages
+'in the Windows 10 Settings app on the Creators Update and newer.
+'Copyright (C) 2017  Drew Naylor
+'Microsoft Windows and all related words are copyright
+'and trademark Microsoft Corporation.
+'(Note that the copyright years include the years left out by the hyphen.)
 '
-' This file is part of HideSettingsPages
-' (Program is also known as "Hide Windows 10 Settings App Pages.")
+'This file is part of HideSettingsPages
+'(Program is also known as "Hide Windows 10 Settings App Pages.")
 '
-' HideSetingsPages is free software: you can redistribute it and/or modify
-'   it under the terms of the GNU General Public License as published by
-'   the Free Software Foundation, either version 3 of the License, or
-'   (at your option) any later version.
+'HideSetingsPages is free software: you can redistribute it and/or modify
+'it under the terms of the GNU General Public License as published by
+'the Free Software Foundation, either version 3 of the License, or
+'(at your option) any later version.
 '
-' HideSettingsPages is distributed in the hope that it will be useful,
-'   but WITHOUT ANY WARRANTY; without even the implied warranty of
-'   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-'   GNU General Public License for more details.
+'HideSettingsPages is distributed in the hope that it will be useful,
+'but WITHOUT ANY WARRANTY; without even the implied warranty of
+'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'GNU General Public License for more details.
 '
-' You should have received a copy of the GNU General Public License
-'   along with HideSettingsPages.  If not, see <http://www.gnu.org/licenses/>.
+'You should have received a copy of the GNU General Public License
+'along with HideSettingsPages.  If not, see <http://www.gnu.org/licenses/>.
 
 
 
