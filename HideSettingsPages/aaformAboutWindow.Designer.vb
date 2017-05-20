19287fc7784d29360b19d0d840340de077990b61
Drew Naylor <DrewNaylor@users.noreply.github.com>
2017-05-19 23:21:55 -0400

    Adding the UXL Launcher About window...
    template to this project. There are many problems with it, but this is
    step 1.

---
+++ HideSettingsPages/aaformAboutWindow.Designer.vb
@@ -0,0 +1,306 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class aaformAboutWindow
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(aaformAboutWindow))
+        Me.tableLayoutPanel = New System.Windows.Forms.TableLayoutPanel()
+        Me.buttonClose = New System.Windows.Forms.Button()
+        Me.tabcontrolAboutWindow = New System.Windows.Forms.TabControl()
+        Me.tabpageAbout = New System.Windows.Forms.TabPage()
+        Me.tableLayoutPanelAboutApp = New System.Windows.Forms.TableLayoutPanel()
+        Me.pictureboxUXLBanner = New System.Windows.Forms.PictureBox()
+        Me.flowLayoutPanelButtons = New System.Windows.Forms.FlowLayoutPanel()
+        Me.linkRepository = New System.Windows.Forms.LinkLabel()
+        Me.linkUpdateCheck = New System.Windows.Forms.LinkLabel()
+        Me.linkMyWebsite = New System.Windows.Forms.LinkLabel()
+        Me.textboxAboutApp = New System.Windows.Forms.TextBox()
+        Me.tabpageLicense = New System.Windows.Forms.TabPage()
+        Me.rtftextboxLicense = New System.Windows.Forms.RichTextBox()
+        Me.tabpageAcknowledgments = New System.Windows.Forms.TabPage()
+        Me.rtftextboxAcknowledgements = New System.Windows.Forms.RichTextBox()
+        Me.tableLayoutPanel.SuspendLayout()
+        Me.tabcontrolAboutWindow.SuspendLayout()
+        Me.tabpageAbout.SuspendLayout()
+        Me.tableLayoutPanelAboutApp.SuspendLayout()
+        CType(Me.pictureboxUXLBanner, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.flowLayoutPanelButtons.SuspendLayout()
+        Me.tabpageLicense.SuspendLayout()
+        Me.tabpageAcknowledgments.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tableLayoutPanel
+        '
+        Me.tableLayoutPanel.ColumnCount = 4
+        Me.tableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.96255!))
+        Me.tableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 44.37086!))
+        Me.tableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.60706!))
+        Me.tableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 280.0!))
+        Me.tableLayoutPanel.Controls.Add(Me.buttonClose, 3, 1)
+        Me.tableLayoutPanel.Controls.Add(Me.tabcontrolAboutWindow, 0, 0)
+        Me.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tableLayoutPanel.Location = New System.Drawing.Point(0, 0)
+        Me.tableLayoutPanel.Margin = New System.Windows.Forms.Padding(6)
+        Me.tableLayoutPanel.Name = "tableLayoutPanel"
+        Me.tableLayoutPanel.RowCount = 2
+        Me.tableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 93.20595!))
+        Me.tableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.794055!))
+        Me.tableLayoutPanel.Size = New System.Drawing.Size(1068, 942)
+        Me.tableLayoutPanel.TabIndex = 0
+        '
+        'buttonClose
+        '
+        Me.buttonClose.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.buttonClose.AutoSize = True
+        Me.buttonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.buttonClose.Location = New System.Drawing.Point(908, 884)
+        Me.buttonClose.Margin = New System.Windows.Forms.Padding(6)
+        Me.buttonClose.Name = "buttonClose"
+        Me.buttonClose.Size = New System.Drawing.Size(154, 52)
+        Me.buttonClose.TabIndex = 0
+        Me.buttonClose.Text = "Close"
+        Me.buttonClose.UseVisualStyleBackColor = True
+        '
+        'tabcontrolAboutWindow
+        '
+        Me.tableLayoutPanel.SetColumnSpan(Me.tabcontrolAboutWindow, 4)
+        Me.tabcontrolAboutWindow.Controls.Add(Me.tabpageAbout)
+        Me.tabcontrolAboutWindow.Controls.Add(Me.tabpageLicense)
+        Me.tabcontrolAboutWindow.Controls.Add(Me.tabpageAcknowledgments)
+        Me.tabcontrolAboutWindow.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tabcontrolAboutWindow.Location = New System.Drawing.Point(6, 6)
+        Me.tabcontrolAboutWindow.Margin = New System.Windows.Forms.Padding(6)
+        Me.tabcontrolAboutWindow.Name = "tabcontrolAboutWindow"
+        Me.tabcontrolAboutWindow.SelectedIndex = 0
+        Me.tabcontrolAboutWindow.Size = New System.Drawing.Size(1056, 866)
+        Me.tabcontrolAboutWindow.TabIndex = 2
+        '
+        'tabpageAbout
+        '
+        Me.tabpageAbout.Controls.Add(Me.tableLayoutPanelAboutApp)
+        Me.tabpageAbout.Location = New System.Drawing.Point(8, 39)
+        Me.tabpageAbout.Margin = New System.Windows.Forms.Padding(6)
+        Me.tabpageAbout.Name = "tabpageAbout"
+        Me.tabpageAbout.Padding = New System.Windows.Forms.Padding(6)
+        Me.tabpageAbout.Size = New System.Drawing.Size(1040, 819)
+        Me.tabpageAbout.TabIndex = 0
+        Me.tabpageAbout.Text = "About"
+        Me.tabpageAbout.UseVisualStyleBackColor = True
+        '
+        'tableLayoutPanelAboutApp
+        '
+        Me.tableLayoutPanelAboutApp.ColumnCount = 3
+        Me.tableLayoutPanelAboutApp.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 32.49027!))
+        Me.tableLayoutPanelAboutApp.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.72374!))
+        Me.tableLayoutPanelAboutApp.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.98055!))
+        Me.tableLayoutPanelAboutApp.Controls.Add(Me.pictureboxUXLBanner, 0, 0)
+        Me.tableLayoutPanelAboutApp.Controls.Add(Me.flowLayoutPanelButtons, 1, 5)
+        Me.tableLayoutPanelAboutApp.Controls.Add(Me.textboxAboutApp, 1, 0)
+        Me.tableLayoutPanelAboutApp.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tableLayoutPanelAboutApp.Location = New System.Drawing.Point(6, 6)
+        Me.tableLayoutPanelAboutApp.Margin = New System.Windows.Forms.Padding(6)
+        Me.tableLayoutPanelAboutApp.Name = "tableLayoutPanelAboutApp"
+        Me.tableLayoutPanelAboutApp.RowCount = 6
+        Me.tableLayoutPanelAboutApp.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.374999!))
+        Me.tableLayoutPanelAboutApp.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.374999!))
+        Me.tableLayoutPanelAboutApp.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.374999!))
+        Me.tableLayoutPanelAboutApp.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.374999!))
+        Me.tableLayoutPanelAboutApp.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.12469!))
+        Me.tableLayoutPanelAboutApp.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.96758!))
+        Me.tableLayoutPanelAboutApp.Size = New System.Drawing.Size(1028, 807)
+        Me.tableLayoutPanelAboutApp.TabIndex = 1
+        '
+        'pictureboxUXLBanner
+        '
+        Me.pictureboxUXLBanner.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pictureboxUXLBanner.Image = Global.HideSettingsPages.My.Resources.Resources.UXL_Launcher_Banner
+        Me.pictureboxUXLBanner.Location = New System.Drawing.Point(6, 6)
+        Me.pictureboxUXLBanner.Margin = New System.Windows.Forms.Padding(6)
+        Me.pictureboxUXLBanner.Name = "pictureboxUXLBanner"
+        Me.tableLayoutPanelAboutApp.SetRowSpan(Me.pictureboxUXLBanner, 5)
+        Me.pictureboxUXLBanner.Size = New System.Drawing.Size(321, 690)
+        Me.pictureboxUXLBanner.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pictureboxUXLBanner.TabIndex = 0
+        Me.pictureboxUXLBanner.TabStop = False
+        '
+        'flowLayoutPanelButtons
+        '
+        Me.tableLayoutPanelAboutApp.SetColumnSpan(Me.flowLayoutPanelButtons, 2)
+        Me.flowLayoutPanelButtons.Controls.Add(Me.linkRepository)
+        Me.flowLayoutPanelButtons.Controls.Add(Me.linkUpdateCheck)
+        Me.flowLayoutPanelButtons.Controls.Add(Me.linkMyWebsite)
+        Me.flowLayoutPanelButtons.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.flowLayoutPanelButtons.Location = New System.Drawing.Point(339, 708)
+        Me.flowLayoutPanelButtons.Margin = New System.Windows.Forms.Padding(6)
+        Me.flowLayoutPanelButtons.Name = "flowLayoutPanelButtons"
+        Me.flowLayoutPanelButtons.Size = New System.Drawing.Size(683, 93)
+        Me.flowLayoutPanelButtons.TabIndex = 5
+        '
+        'linkRepository
+        '
+        Me.linkRepository.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.linkRepository.AutoSize = True
+        Me.linkRepository.Location = New System.Drawing.Point(6, 20)
+        Me.linkRepository.Margin = New System.Windows.Forms.Padding(6, 20, 6, 0)
+        Me.linkRepository.Name = "linkRepository"
+        Me.linkRepository.Size = New System.Drawing.Size(226, 25)
+        Me.linkRepository.TabIndex = 1
+        Me.linkRepository.TabStop = True
+        Me.linkRepository.Text = "Visit GitHub repository"
+        '
+        'linkUpdateCheck
+        '
+        Me.linkUpdateCheck.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.linkUpdateCheck.AutoSize = True
+        Me.linkUpdateCheck.Location = New System.Drawing.Point(244, 20)
+        Me.linkUpdateCheck.Margin = New System.Windows.Forms.Padding(6, 20, 6, 0)
+        Me.linkUpdateCheck.Name = "linkUpdateCheck"
+        Me.linkUpdateCheck.Size = New System.Drawing.Size(187, 25)
+        Me.linkUpdateCheck.TabIndex = 0
+        Me.linkUpdateCheck.TabStop = True
+        Me.linkUpdateCheck.Text = "Check for updates"
+        '
+        'linkMyWebsite
+        '
+        Me.linkMyWebsite.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.linkMyWebsite.AutoSize = True
+        Me.linkMyWebsite.Location = New System.Drawing.Point(443, 20)
+        Me.linkMyWebsite.Margin = New System.Windows.Forms.Padding(6, 20, 6, 0)
+        Me.linkMyWebsite.Name = "linkMyWebsite"
+        Me.linkMyWebsite.Size = New System.Drawing.Size(202, 25)
+        Me.linkMyWebsite.TabIndex = 2
+        Me.linkMyWebsite.TabStop = True
+        Me.linkMyWebsite.Text = "Visit Drew's website"
+        '
+        'textboxAboutApp
+        '
+        Me.textboxAboutApp.BackColor = System.Drawing.SystemColors.Window
+        Me.tableLayoutPanelAboutApp.SetColumnSpan(Me.textboxAboutApp, 2)
+        Me.textboxAboutApp.Location = New System.Drawing.Point(345, 6)
+        Me.textboxAboutApp.Margin = New System.Windows.Forms.Padding(12, 6, 6, 6)
+        Me.textboxAboutApp.Multiline = True
+        Me.textboxAboutApp.Name = "textboxAboutApp"
+        Me.textboxAboutApp.ReadOnly = True
+        Me.tableLayoutPanelAboutApp.SetRowSpan(Me.textboxAboutApp, 5)
+        Me.textboxAboutApp.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.textboxAboutApp.Size = New System.Drawing.Size(674, 678)
+        Me.textboxAboutApp.TabIndex = 0
+        Me.textboxAboutApp.TabStop = False
+        Me.textboxAboutApp.Text = resources.GetString("textboxAboutApp.Text")
+        '
+        'tabpageLicense
+        '
+        Me.tabpageLicense.Controls.Add(Me.rtftextboxLicense)
+        Me.tabpageLicense.Location = New System.Drawing.Point(8, 39)
+        Me.tabpageLicense.Name = "tabpageLicense"
+        Me.tabpageLicense.Padding = New System.Windows.Forms.Padding(3)
+        Me.tabpageLicense.Size = New System.Drawing.Size(1040, 819)
+        Me.tabpageLicense.TabIndex = 3
+        Me.tabpageLicense.Text = "License"
+        Me.tabpageLicense.UseVisualStyleBackColor = True
+        '
+        'rtftextboxLicense
+        '
+        Me.rtftextboxLicense.BackColor = System.Drawing.SystemColors.Window
+        Me.rtftextboxLicense.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.rtftextboxLicense.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.rtftextboxLicense.Location = New System.Drawing.Point(3, 3)
+        Me.rtftextboxLicense.Margin = New System.Windows.Forms.Padding(6)
+        Me.rtftextboxLicense.Name = "rtftextboxLicense"
+        Me.rtftextboxLicense.ReadOnly = True
+        Me.rtftextboxLicense.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical
+        Me.rtftextboxLicense.Size = New System.Drawing.Size(1034, 813)
+        Me.rtftextboxLicense.TabIndex = 0
+        Me.rtftextboxLicense.Text = ""
+        '
+        'tabpageAcknowledgments
+        '
+        Me.tabpageAcknowledgments.Controls.Add(Me.rtftextboxAcknowledgements)
+        Me.tabpageAcknowledgments.Location = New System.Drawing.Point(8, 39)
+        Me.tabpageAcknowledgments.Name = "tabpageAcknowledgments"
+        Me.tabpageAcknowledgments.Padding = New System.Windows.Forms.Padding(3)
+        Me.tabpageAcknowledgments.Size = New System.Drawing.Size(1040, 819)
+        Me.tabpageAcknowledgments.TabIndex = 4
+        Me.tabpageAcknowledgments.Text = "Acknowledgments"
+        Me.tabpageAcknowledgments.UseVisualStyleBackColor = True
+        '
+        'rtftextboxAcknowledgements
+        '
+        Me.rtftextboxAcknowledgements.BackColor = System.Drawing.SystemColors.Window
+        Me.rtftextboxAcknowledgements.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.rtftextboxAcknowledgements.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.rtftextboxAcknowledgements.Location = New System.Drawing.Point(3, 3)
+        Me.rtftextboxAcknowledgements.Margin = New System.Windows.Forms.Padding(6)
+        Me.rtftextboxAcknowledgements.Name = "rtftextboxAcknowledgements"
+        Me.rtftextboxAcknowledgements.ReadOnly = True
+        Me.rtftextboxAcknowledgements.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical
+        Me.rtftextboxAcknowledgements.Size = New System.Drawing.Size(1034, 813)
+        Me.rtftextboxAcknowledgements.TabIndex = 1
+        Me.rtftextboxAcknowledgements.Text = ""
+        '
+        'aaformAboutWindow
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(192.0!, 192.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.CancelButton = Me.buttonClose
+        Me.ClientSize = New System.Drawing.Size(1068, 942)
+        Me.Controls.Add(Me.tableLayoutPanel)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(6)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "aaformAboutWindow"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "About UXL Launcher"
+        Me.tableLayoutPanel.ResumeLayout(False)
+        Me.tableLayoutPanel.PerformLayout()
+        Me.tabcontrolAboutWindow.ResumeLayout(False)
+        Me.tabpageAbout.ResumeLayout(False)
+        Me.tableLayoutPanelAboutApp.ResumeLayout(False)
+        Me.tableLayoutPanelAboutApp.PerformLayout()
+        CType(Me.pictureboxUXLBanner, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.flowLayoutPanelButtons.ResumeLayout(False)
+        Me.flowLayoutPanelButtons.PerformLayout()
+        Me.tabpageLicense.ResumeLayout(False)
+        Me.tabpageAcknowledgments.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents tableLayoutPanel As TableLayoutPanel
+    Friend WithEvents buttonClose As Button
+    Friend WithEvents tabcontrolAboutWindow As TabControl
+    Friend WithEvents tabpageAbout As TabPage
+    Friend WithEvents pictureboxUXLBanner As PictureBox
+    Friend WithEvents tableLayoutPanelAboutApp As TableLayoutPanel
+    Friend WithEvents textboxAboutApp As TextBox
+    Friend WithEvents flowLayoutPanelButtons As FlowLayoutPanel
+    Friend WithEvents linkUpdateCheck As LinkLabel
+    Friend WithEvents linkRepository As LinkLabel
+    Friend WithEvents linkMyWebsite As LinkLabel
+    Friend WithEvents tabpageLicense As TabPage
+    Friend WithEvents tabpageAcknowledgments As TabPage
+    Friend WithEvents rtftextboxLicense As RichTextBox
+    Friend WithEvents rtftextboxAcknowledgements As RichTextBox
+End Class
---
+++ HideSettingsPages/aaformAboutWindow.vb
@@ -0,0 +1,131 @@
+﻿'UXL Launcher - UXL Launcher provides launchers for most Microsoft Office apps in one place.
+'Copyright (C) 2013-2017  Drew Naylor
+'Microsoft Office and all related words are copyright
+'and trademark Microsoft Corporation.
+'(Note that the copyright years include the years left out by the hyphen.)
+'
+'This file is part of UXL Launcher
+'(Program is also known as "Unified eXecutable Launcher." Not to be confused with
+'another software titled "[Kindle] Unified Application Launcher".)
+'
+'UXL Launcher is free software: you can redistribute it and/or modify
+'it under the terms of the GNU General Public License as published by
+'the Free Software Foundation, either version 3 of the License, or
+'(at your option) any later version.
+'
+'UXL Launcher is distributed in the hope that it will be useful,
+'but WITHOUT ANY WARRANTY; without even the implied warranty of
+'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'GNU General Public License for more details.
+'
+'You should have received a copy of the GNU General Public License
+'along with UXL Launcher.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+Imports System.ComponentModel
+
+Public Class aaformAboutWindow
+#Region "Code that runs when the About window is opened."
+    Private Sub RealAboutWindow_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Create a string with the BuildDate.txt file.
+        Dim BuildDateString As String = My.Resources.BuildDate
+        BuildDateString = BuildDateString.TrimEnd(CType(vbCrLf, Char()))
+
+        ' Put the text in the About box on launch.
+        textboxAboutApp.Text = ("UXL Launcher - Unified eXecutable Launcher" & vbCrLf &
+"Version " & My.Application.Info.Version.ToString & " Git - Codename ""Personalizationizer 3000""" & vbCrLf &
+"App compiled at UTC: " & BuildDateString & vbCrLf &
+"Copyright (C) 2013-2017  Drew Naylor" & vbCrLf &
+"" & vbCrLf &
+"UXL Launcher provides launchers for most Microsoft Office apps in one place." & vbCrLf &
+"" & vbCrLf &
+"License notice:" & vbCrLf &
+"UXL Launcher is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by" &
+" the Free Software Foundation, either version 3 of the License, or (at your option) any later version." & vbCrLf &
+"" & vbCrLf &
+"UXL Launcher is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details." & vbCrLf &
+"" & vbCrLf &
+"You should have received a copy of the GNU General Public License along with UXL Launcher. If not, see <http://www.gnu.org/licenses/>." & vbCrLf &
+"" & vbCrLf &
+"Drew is not intending on infringing on Microsoft" & "'" & "s copyrights, so UXL Launcher is only a shortcut application." & vbCrLf &
+"" & vbCrLf &
+"Copyright notice: Office, Microsoft Office, Word, Excel, PowerPoint, And all related words (by MSFT) are Copyright [a long time ago]-[sometime in the future] Microsoft Corp. All Rights Reserved for Microsoft" & "'" & "s copyrights." & vbCrLf &
+"Any other companies mentioned own their respective copyrights/trademarks.")
+
+
+        ' Load the GPL document in the RTF Textbox control.
+        rtftextboxLicense.Rtf = My.Resources.gpl_RTF
+        ' Load the Acknowledgements document in the RTF Textbox control.
+        rtftextboxAcknowledgements.Rtf = My.Resources.Acknowledgements_RTF
+
+    End Sub
+#End Region
+#Region "Code to run when clicking the link to go to the GitHub repository."
+    Private Sub linkRepository_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles linkRepository.LinkClicked
+        ' Open the GitHub repository.
+        Process.Start("https://github.com/DrewNaylor/UXL-Launcher")
+    End Sub
+#End Region
+#Region "Code to run when clicking the link to check for updates."
+    Private Sub linkUpdateCheck_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles linkUpdateCheck.LinkClicked
+        ' Go to the Releases page.
+        Process.Start("https://github.com/DrewNaylor/UXL-Launcher/releases/latest")
+    End Sub
+#End Region
+#Region "Code to run when clicking the link to go to my website."
+    Private Sub linkMyWebsite_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles linkMyWebsite.LinkClicked
+        ' Go to my website.
+        Process.Start("http://drewnaylor.github.io")
+    End Sub
+#End Region
+#Region "Code to run when closing the About window."
+    Private Sub aaformAboutWindow_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+
+        ' When the Options window is closing, tell the main window to be Always On Top if My.Settings.alwaysOnTop is set to True.
+        If My.Settings.alwaysOnTop = True Then
+            aaformMainWindow.TopMost = True
+        ElseIf My.Settings.alwaysOnTop = False Then
+            aaformMainWindow.TopMost = False
+        End If
+
+        ' Debug label for the Always On Top feature.
+        aaformMainWindow.debugLabelForAlwaysOnTop.Text = "menubar button checkstate: " & aaformMainWindow.menubarAlwaysOnTopButton.CheckState & vbNewLine &
+        "alwaysOnTop setting: " & My.Settings.alwaysOnTop & vbNewLine &
+        "main window TopMost: " & aaformMainWindow.TopMost
+    End Sub
+#End Region
+#Region "Code to run when clicking links in the RTF documents."
+    Private Sub rtftextboxLicense_LinkClicked(sender As Object, e As LinkClickedEventArgs) Handles rtftextboxLicense.LinkClicked
+        ' When the user clicks a link in the RTF License rich textbox, go to that
+        ' page in the default browser if the user chooses to visit the
+        ' page in the messagebox.
+        Dim msgResult As Integer = MessageBox.Show("Are you sure you want open this link?" & vbCrLf &
+                "" & vbCrLf &
+                e.LinkText, "UXL Launcher",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+
+        ' If the user chooses to open the link, navigate the default browser there.
+        If msgResult = DialogResult.Yes Then
+            Process.Start(e.LinkText)
+        End If
+    End Sub
+
+    Private Sub rtftextboxAcknowledgements_LinkClicked(sender As Object, e As LinkClickedEventArgs) Handles rtftextboxAcknowledgements.LinkClicked
+        ' When the user clicks a link in the RTF License rich textbox, go to that
+        ' page in the default browser if the user chooses to visit the
+        ' page in the messagebox.
+        Dim msgResult As Integer = MessageBox.Show("Are you sure you want open this link?" & vbCrLf &
+                "" & vbCrLf &
+                e.LinkText, "UXL Launcher",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+
+        ' If the user chooses to open the link, navigate the default browser there.
+        If msgResult = DialogResult.Yes Then
+            Process.Start(e.LinkText)
+        End If
+    End Sub
+#End Region
+
+End Class
