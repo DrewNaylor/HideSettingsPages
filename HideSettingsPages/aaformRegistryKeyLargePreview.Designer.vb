e33a7fa7143c07580076273642ac512a8859addb
Drew Naylor <DrewNaylor@users.noreply.github.com>
2017-05-19 22:35:58 -0400

    add textbox to the other window.

--- HideSettingsPages/aaformRegistryKeyLargePreview.Designer.vb
+++ HideSettingsPages/aaformRegistryKeyLargePreview.Designer.vb
@@ -22,17 +22,30 @@ Partial Class aaformRegistryKeyLargePreview
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(284, 262)
+        Me.TextBox1.TabIndex = 0
         '
         'aaformRegistryKeyLargePreview
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
         Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.TextBox1)
         Me.Name = "aaformRegistryKeyLargePreview"
         Me.Text = "aaformRegistryKeyLargePreview"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-    Friend PreviewRegistryKey1 As previewRegistryKey
+    Friend WithEvents TextBox1 As TextBox
 End Class
