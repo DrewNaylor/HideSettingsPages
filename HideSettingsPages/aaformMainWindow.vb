59c012c98905c7c756f3cf97648dcd92a51e5407
Drew Naylor <DrewNaylor@users.noreply.github.com>
2017-08-03 20:14:18 -0400

    Fix typos + upgrade hsp_registry-helper...
    version number to 0.2.

--- HideSettingsPages/aaformMainWindow.vb
+++ HideSettingsPages/aaformMainWindow.vb
@@ -307,7 +307,7 @@ Public Class aaformMainWindow
 
                     ' Put another messagebox in so that the user
                     ' has enough time to make a restore point.
-                    MessageBox.Show("Once you're ready to apply the key value, click OK.", "Apply key value")
+                    MessageBox.Show("Once you're ready to apply the key value, click OK.", "Undo all changes")
 
                 Case MsgBoxResult.No
                     ' This just skips this code.
@@ -396,7 +396,7 @@ Public Class aaformMainWindow
 
                     ' Put another messagebox in so that the user
                     ' has enough time to make a restore point.
-                    MessageBox.Show("Once you're ready to apply the key value, click OK.", "Apply key value")
+                    MessageBox.Show("Once you're ready to apply the key value, click OK.", "Apply changes")
 
                 Case MsgBoxResult.No
                     ' This just skips this code.
