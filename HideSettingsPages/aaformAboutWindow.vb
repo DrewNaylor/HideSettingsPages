b621896e7ddd61dda0e71e1a4a37759cb95e4fd4
Drew Naylor <DrewNaylor@users.noreply.github.com>
2017-08-18 19:48:33 -0400

    Fix typo + delete old screenshot.

--- HideSettingsPages/aaformAboutWindow.vb
+++ HideSettingsPages/aaformAboutWindow.vb
@@ -12,7 +12,7 @@
 'Original file on GitHub: 
 '<https://github.com/DrewNaylor/UXL-Launcher/blob/master/UXL-Launcher/AboutWindow.vb>
 '
-'HideSetingsPages is free software: you can redistribute it and/or modify
+'HideSettingsPages is free software: you can redistribute it and/or modify
 'it under the terms of the GNU General Public License as published by
 'the Free Software Foundation, either version 3 of the License, or
 '(at your option) any later version.
--- HideSettingsPages/aaformRegistryKeyValueLargePreview.vb
+++ HideSettingsPages/aaformRegistryKeyValueLargePreview.vb
@@ -8,7 +8,7 @@
 'This file is part of HideSettingsPages
 '(Program is also known as "Hide Windows 10 Settings App Pages.")
 '
-'HideSetingsPages is free software: you can redistribute it and/or modify
+'HideSettingsPages is free software: you can redistribute it and/or modify
 'it under the terms of the GNU General Public License as published by
 'the Free Software Foundation, either version 3 of the License, or
 '(at your option) any later version.
