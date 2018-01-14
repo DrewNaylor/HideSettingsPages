56d5d907296a195c70584bc41a89b4751e0f6bb0
Drew Naylor <DrewNaylor@users.noreply.github.com>
2018-01-13 19:08:08 -0500

    Change AutoScaleMode to Dpi for HiDPI devices.

--- HideSettingsPages/aaformDirectlyApplyKeyValue.Designer.vb
+++ HideSettingsPages/aaformDirectlyApplyKeyValue.Designer.vb
@@ -35,8 +35,8 @@ Partial Class aaformDirectlyApplyKeyValue
         '
         'aaformDirectlyApplyKeyValue
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
         Me.ClientSize = New System.Drawing.Size(412, 184)
         Me.Controls.Add(Me.flowlayoutpanelBox)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
