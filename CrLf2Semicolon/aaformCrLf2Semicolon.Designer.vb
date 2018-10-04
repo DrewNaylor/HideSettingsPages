aaeaeebaf08be36843a06660731d0ef921aa3275
Drew Naylor <DrewNaylor@users.noreply.github.com>
2018-10-03 21:18:02 -0400

    Add input textbox and input label.

--- CrLf2Semicolon/aaformCrLf2Semicolon.Designer.vb
+++ CrLf2Semicolon/aaformCrLf2Semicolon.Designer.vb
@@ -22,17 +22,41 @@ Partial Class aaformCrLf2Semicolon
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.labelInput = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 33)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(256, 277)
+        Me.TextBox1.TabIndex = 0
+        '
+        'labelInput
+        '
+        Me.labelInput.AutoSize = True
+        Me.labelInput.Location = New System.Drawing.Point(13, 14)
+        Me.labelInput.Name = "labelInput"
+        Me.labelInput.Size = New System.Drawing.Size(263, 13)
+        Me.labelInput.TabIndex = 1
+        Me.labelInput.Text = "Input text with CrLf (Windows line endings) characters:"
         '
         'aaformCrLf2Semicolon
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
-        Me.ClientSize = New System.Drawing.Size(661, 302)
+        Me.ClientSize = New System.Drawing.Size(661, 367)
+        Me.Controls.Add(Me.labelInput)
+        Me.Controls.Add(Me.TextBox1)
         Me.Name = "aaformCrLf2Semicolon"
         Me.Text = "CrLf2Semicolon"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents labelInput As Label
 End Class
