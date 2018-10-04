b1922471d52f535aafcc08a109857cbdd6678cf9
Drew Naylor <DrewNaylor@users.noreply.github.com>
2018-10-03 21:29:37 -0400

    Select all buttons work.

--- CrLf2Semicolon/aaformCrLf2Semicolon.vb
+++ CrLf2Semicolon/aaformCrLf2Semicolon.vb
@@ -1,3 +1,13 @@
 ﻿Public Class aaformCrLf2Semicolon
+    Private Sub buttonSelectInput_Click(sender As Object, e As EventArgs) Handles buttonSelectInput.Click
+        ' Select all text in Input textbox when clicked.
+        textboxInput.Focus()
+        textboxInput.SelectAll()
+    End Sub
 
+    Private Sub buttonSelectOutput_Click(sender As Object, e As EventArgs) Handles buttonSelectOutput.Click
+        ' Select all text in Output textbox when clicked.
+        textboxOutput.Focus()
+        textboxOutput.SelectAll()
+    End Sub
 End Class
