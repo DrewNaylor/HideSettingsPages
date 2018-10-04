d6b5ee985ba8fa57462cb761d3155bf2c582800b
Drew Naylor <DrewNaylor@users.noreply.github.com>
2018-10-03 21:21:42 -0400

    Add output textbox and label.

--- CrLf2Semicolon/aaformCrLf2Semicolon.Designer.vb
+++ CrLf2Semicolon/aaformCrLf2Semicolon.Designer.vb
@@ -25,6 +25,8 @@ Partial Class aaformCrLf2Semicolon
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.labelInput = New System.Windows.Forms.Label()
         Me.buttonReplaceCrLfWithSemicolons = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'TextBox1
@@ -46,18 +48,37 @@ Partial Class aaformCrLf2Semicolon
         '
         'buttonReplaceCrLfWithSemicolons
         '
-        Me.buttonReplaceCrLfWithSemicolons.Location = New System.Drawing.Point(274, 138)
+        Me.buttonReplaceCrLfWithSemicolons.Location = New System.Drawing.Point(274, 53)
         Me.buttonReplaceCrLfWithSemicolons.Name = "buttonReplaceCrLfWithSemicolons"
-        Me.buttonReplaceCrLfWithSemicolons.Size = New System.Drawing.Size(118, 55)
+        Me.buttonReplaceCrLfWithSemicolons.Size = New System.Drawing.Size(130, 55)
         Me.buttonReplaceCrLfWithSemicolons.TabIndex = 2
         Me.buttonReplaceCrLfWithSemicolons.Text = "Replace CrLf characters with semicolons    --->"
         Me.buttonReplaceCrLfWithSemicolons.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(411, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(119, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Output with semicolons:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(410, 33)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(256, 100)
+        Me.TextBox2.TabIndex = 3
         '
         'aaformCrLf2Semicolon
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
-        Me.ClientSize = New System.Drawing.Size(661, 367)
+        Me.ClientSize = New System.Drawing.Size(678, 367)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.buttonReplaceCrLfWithSemicolons)
         Me.Controls.Add(Me.labelInput)
         Me.Controls.Add(Me.TextBox1)
@@ -71,4 +92,6 @@ Partial Class aaformCrLf2Semicolon
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents labelInput As Label
     Friend WithEvents buttonReplaceCrLfWithSemicolons As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
 End Class
