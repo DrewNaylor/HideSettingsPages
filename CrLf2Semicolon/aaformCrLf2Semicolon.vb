73700107f688ff3aa452405d9374507602b5e75d
Drew Naylor <DrewNaylor@users.noreply.github.com>
2018-10-03 21:40:54 -0400

    Assign temp string to Input textbox...
    when creating temp string.

--- CrLf2Semicolon/aaformCrLf2Semicolon.vb
+++ CrLf2Semicolon/aaformCrLf2Semicolon.vb
@@ -15,20 +15,20 @@
         ' Take the text in the Input textbox and replace
         ' all vbCrLf characters with semicolons, then
         ' output the text in the Output textbox.
-        Dim stringCrLfReplacedBySemicolons As String = ""
+        Dim stringCrLfReplacedBySemicolons As String = textboxInput.Text
         ' If the "Also convert Lf characters" checkbox
         ' is checked, convert vbLf characters to vbCrLf
         ' characters before replacing vbCrLf with
         ' semicolons.
         If checkboxConvertLf.Checked = True Then
-            ' Assign a temporary variable to the text in the
-            ' Input column and replace vbLf characters with
-            ' vbCrLf characters at the same time.
+            ' Replace vbLf characters in temp string with
+            ' vbCrLf characters.
 
             ' This is only if the "Also convert Lf characters"
             ' checkbox is checked.
-            stringCrLfReplacedBySemicolons = textboxInput.Text.Replace(vbLf, vbCrLf)
-
+            stringCrLfReplacedBySemicolons = stringCrLfReplacedBySemicolons.Replace(vbLf, vbCrLf)
         End If
+        ' Replace vbCrLf characters in stringCrLfReplacedBySemicolons
+        ' with semicolons.
     End Sub
 End Class
