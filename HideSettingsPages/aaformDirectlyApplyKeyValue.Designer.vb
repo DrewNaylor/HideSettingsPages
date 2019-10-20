d4cd040de9ec394b66eb8c5e724ab812b0c0baf4
Drew Naylor <DrewNaylor@users.noreply.github.com>
2019-10-19 22:23:54 -0400

    Nope, not doing that with having a panel.

--- HideSettingsPages/aaformDirectlyApplyKeyValue.Designer.vb
+++ HideSettingsPages/aaformDirectlyApplyKeyValue.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class aaformDirectlyApplyKeyValue
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,21 +20,17 @@ Partial Class aaformDirectlyApplyKeyValue
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.labelTypePageOrList = New System.Windows.Forms.Label()
         Me.labelValue = New System.Windows.Forms.Label()
         Me.textboxKeyValue = New System.Windows.Forms.TextBox()
-        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
-        Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.TableLayoutPanel1.SuspendLayout()
-        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'labelTypePageOrList
         '
         Me.labelTypePageOrList.AutoSize = True
-        Me.labelTypePageOrList.Location = New System.Drawing.Point(9, 6)
+        Me.labelTypePageOrList.Location = New System.Drawing.Point(12, 9)
         Me.labelTypePageOrList.Name = "labelTypePageOrList"
         Me.labelTypePageOrList.Size = New System.Drawing.Size(489, 68)
         Me.labelTypePageOrList.TabIndex = 0
@@ -45,7 +41,7 @@ Partial Class aaformDirectlyApplyKeyValue
         'labelValue
         '
         Me.labelValue.AutoSize = True
-        Me.labelValue.Location = New System.Drawing.Point(9, 108)
+        Me.labelValue.Location = New System.Drawing.Point(12, 111)
         Me.labelValue.Name = "labelValue"
         Me.labelValue.Size = New System.Drawing.Size(74, 17)
         Me.labelValue.TabIndex = 1
@@ -53,44 +49,19 @@ Partial Class aaformDirectlyApplyKeyValue
         '
         'textboxKeyValue
         '
-        Me.textboxKeyValue.Location = New System.Drawing.Point(90, 105)
+        Me.textboxKeyValue.Location = New System.Drawing.Point(93, 108)
         Me.textboxKeyValue.Name = "textboxKeyValue"
         Me.textboxKeyValue.Size = New System.Drawing.Size(408, 22)
         Me.textboxKeyValue.TabIndex = 2
-        '
-        'TableLayoutPanel1
-        '
-        Me.TableLayoutPanel1.ColumnCount = 1
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-        Me.TableLayoutPanel1.Controls.Add(Me.Panel1, 0, 0)
-        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
-        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
-        Me.TableLayoutPanel1.RowCount = 2
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 76.69903!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 23.30097!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(515, 230)
-        Me.TableLayoutPanel1.TabIndex = 4
-        '
-        'Panel1
-        '
-        Me.Panel1.Controls.Add(Me.textboxKeyValue)
-        Me.Panel1.Controls.Add(Me.labelValue)
-        Me.Panel1.Controls.Add(Me.labelTypePageOrList)
-        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Panel1.Location = New System.Drawing.Point(3, 3)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(509, 170)
-        Me.Panel1.TabIndex = 3
         '
         'aaformDirectlyApplyKeyValue
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(120.0!, 120.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
         Me.ClientSize = New System.Drawing.Size(515, 230)
-        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.textboxKeyValue)
+        Me.Controls.Add(Me.labelValue)
+        Me.Controls.Add(Me.labelTypePageOrList)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.MaximizeBox = False
@@ -99,16 +70,12 @@ Partial Class aaformDirectlyApplyKeyValue
         Me.ShowIcon = False
         Me.ShowInTaskbar = False
         Me.Text = "Directly Apply Key Value"
-        Me.TableLayoutPanel1.ResumeLayout(False)
-        Me.Panel1.ResumeLayout(False)
-        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents labelTypePageOrList As Label
     Friend WithEvents labelValue As Label
     Friend WithEvents textboxKeyValue As TextBox
-    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
-    Friend WithEvents Panel1 As Panel
 End Class
