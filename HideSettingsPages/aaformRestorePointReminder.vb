5a3b2d90bf54f558bc3c9243b9786e8636bfcefc
Drew Naylor <DrewNaylor@users.noreply.github.com>
2019-10-21 21:33:57 -0400

    Trying to put in the code for the reminder window.
    Can't get it to work with sysnative, so I may need to do the Control Panel thing still.

--- HideSettingsPages/aaformRestorePointReminder.vb
+++ HideSettingsPages/aaformRestorePointReminder.vb
@@ -1,3 +1,19 @@
 ﻿Public Class aaformRestorePointReminder
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://www.tenforums.com/tutorials/4533-turn-off-system-protection-drives-windows-10-a.html")
+    End Sub
 
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Process.Start("https://www.tenforums.com/tutorials/4571-create-system-restore-point-windows-10-a.html")
+    End Sub
+
+    Private Sub buttonOpenRestorePointControlPanel_Click(sender As Object, e As EventArgs) Handles buttonOpenRestorePointControlPanel.Click
+        Debug.WriteLine("GetFolderPath: {0}", Environment.GetFolderPath(Environment.SpecialFolder.System))
+        Debug.WriteLine("GetFolderPath: {0}", Environment.GetFolderPath(Environment.SpecialFolder.SystemX86))
+        Dim proc As New ProcessStartInfo
+        proc.FileName = "C:\Windows\sysnative\SystemPropertiesProtection.exe"
+        Debug.WriteLine(proc.FileName)
+        'proc.Verb = "runas"
+        Process.Start("C:\Windows\sysnative\SystemPropertiesProtection.exe")
+    End Sub
 End Class
