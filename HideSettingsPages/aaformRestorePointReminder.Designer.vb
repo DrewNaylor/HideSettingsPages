6efb09bc84ab2725061c840dda934b4a9e8b1f4d
Drew Naylor <DrewNaylor@users.noreply.github.com>
2019-10-21 22:58:40 -0400

    Working on replacing current restore point reminder...
    messagebox with the new one that has buttons and linklabels.

--- HideSettingsPages/aaformRestorePointReminder.Designer.vb
+++ HideSettingsPages/aaformRestorePointReminder.Designer.vb
@@ -27,30 +27,31 @@ Partial Class aaformRestorePointReminder
         Me.labelRestorePointReminder = New System.Windows.Forms.Label()
         Me.linklabelTurnOnOrOffSystemProtection = New System.Windows.Forms.LinkLabel()
         Me.linklabelCreateSystemRestorePoint = New System.Windows.Forms.LinkLabel()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'buttonOpenRestorePointControlPanel
         '
-        Me.buttonOpenRestorePointControlPanel.Location = New System.Drawing.Point(275, 164)
+        Me.buttonOpenRestorePointControlPanel.Location = New System.Drawing.Point(273, 260)
         Me.buttonOpenRestorePointControlPanel.Name = "buttonOpenRestorePointControlPanel"
-        Me.buttonOpenRestorePointControlPanel.Size = New System.Drawing.Size(219, 70)
+        Me.buttonOpenRestorePointControlPanel.Size = New System.Drawing.Size(107, 33)
         Me.buttonOpenRestorePointControlPanel.TabIndex = 0
-        Me.buttonOpenRestorePointControlPanel.Text = "Open create restore point window"
+        Me.buttonOpenRestorePointControlPanel.Text = "Yes"
         Me.buttonOpenRestorePointControlPanel.UseVisualStyleBackColor = True
         '
         'labelRestorePointReminder
         '
         Me.labelRestorePointReminder.AutoSize = True
-        Me.labelRestorePointReminder.Location = New System.Drawing.Point(13, 13)
+        Me.labelRestorePointReminder.Location = New System.Drawing.Point(16, 9)
         Me.labelRestorePointReminder.Name = "labelRestorePointReminder"
-        Me.labelRestorePointReminder.Size = New System.Drawing.Size(457, 68)
+        Me.labelRestorePointReminder.Size = New System.Drawing.Size(457, 136)
         Me.labelRestorePointReminder.TabIndex = 1
         Me.labelRestorePointReminder.Text = resources.GetString("labelRestorePointReminder.Text")
         '
         'linklabelTurnOnOrOffSystemProtection
         '
         Me.linklabelTurnOnOrOffSystemProtection.AutoSize = True
-        Me.linklabelTurnOnOrOffSystemProtection.Location = New System.Drawing.Point(16, 91)
+        Me.linklabelTurnOnOrOffSystemProtection.Location = New System.Drawing.Point(16, 175)
         Me.linklabelTurnOnOrOffSystemProtection.Name = "linklabelTurnOnOrOffSystemProtection"
         Me.linklabelTurnOnOrOffSystemProtection.Size = New System.Drawing.Size(346, 34)
         Me.linklabelTurnOnOrOffSystemProtection.TabIndex = 2
@@ -61,22 +62,32 @@ Partial Class aaformRestorePointReminder
         'linklabelCreateSystemRestorePoint
         '
         Me.linklabelCreateSystemRestorePoint.AutoSize = True
-        Me.linklabelCreateSystemRestorePoint.Location = New System.Drawing.Point(16, 132)
+        Me.linklabelCreateSystemRestorePoint.Location = New System.Drawing.Point(16, 216)
         Me.linklabelCreateSystemRestorePoint.Name = "linklabelCreateSystemRestorePoint"
         Me.linklabelCreateSystemRestorePoint.Size = New System.Drawing.Size(477, 17)
         Me.linklabelCreateSystemRestorePoint.TabIndex = 3
         Me.linklabelCreateSystemRestorePoint.TabStop = True
         Me.linklabelCreateSystemRestorePoint.Text = "TenForums: How to Create a System Restore Point in Windows 10 (step 5)"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(386, 260)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(107, 33)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "No"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'aaformRestorePointReminder
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(120.0!, 120.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
-        Me.ClientSize = New System.Drawing.Size(506, 246)
+        Me.ClientSize = New System.Drawing.Size(506, 305)
         Me.Controls.Add(Me.linklabelCreateSystemRestorePoint)
         Me.Controls.Add(Me.linklabelTurnOnOrOffSystemProtection)
         Me.Controls.Add(Me.labelRestorePointReminder)
         Me.Controls.Add(Me.buttonOpenRestorePointControlPanel)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "aaformRestorePointReminder"
         Me.Text = "Restore point reminder"
         Me.ResumeLayout(False)
@@ -88,4 +99,5 @@ Partial Class aaformRestorePointReminder
     Friend WithEvents labelRestorePointReminder As Label
     Friend WithEvents linklabelTurnOnOrOffSystemProtection As LinkLabel
     Friend WithEvents linklabelCreateSystemRestorePoint As LinkLabel
+    Friend WithEvents Button1 As Button
 End Class
